7a087fc41b3c1b8a518b0dcb8668aaccb4388d11
wangyipeng <job18519091637@163.com>
2022-02-17 09:47:48 +0800

    3

--- src/main/java/com/example/demostudy/java/TestStringJoin.java
+++ src/main/java/com/example/demostudy/java/TestStringJoin.java
@@ -1,13 +1,12 @@
 package com.example.demostudy.java;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.StringJoiner;
 
 public class TestStringJoin {
     public static void main(String[] args) {
         List list = Arrays.asList("1", "2", "3");
+        List list2 = Arrays.asList("1", "2", "3");
         List list1 = Arrays.asList("1", "2", "3");
         final String join = String.join(",", list);
         System.out.println(join);
